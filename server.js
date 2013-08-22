9c9e80624a47d8acf1513dbe236db52ebb572d9a
Andu <andu0814@yahoo.com>
2013-08-22 14:38:18 +0300

    res.close() bug fixed

---
+++ server.js
@@ -0,0 +1,26 @@
+PORT = 8002;
+HOST = '127.0.0.1';
+
+var sys = require('sys'),
+fs = require('fs');
+http = require('http');
+
+http.createServer(function(req, res) {
+	fs.readFile('index.html', function(err, data) {
+		if (err) {
+			sys.puts('Error loading file index.html');
+		} else {
+			sys.puts('Loading file index.html');
+		}
+
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+			'Content-Length': data.length
+		});
+		res.write(data);
+		res.end();
+	});
+}).listen(PORT, HOST);
+
+sys.puts("Server at http://" + HOST + ':' + PORT.toString() + '/');
+
